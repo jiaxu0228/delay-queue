3a0fa19048a4e1ea2a95fa23a2e432baae741236
jiaxu0228 <jiaxu0228@qq.com>
2021-03-27 13:13:48 +0800

    第一项目提交

---
+++ src/test/java/com/delay/queue/DelayQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delay.queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelayQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
